91e21eb6aa7c298a71cfa50f4edcf06675bf3b75
Agito <gvagitoblgv@gmail.com>
2020-10-19 15:13:18 +0300

    Added game-counter and game levels

--- index.js
+++ index.js
@@ -1,43 +1,56 @@
 const gameWindow = document.querySelector('.game-window');
+const gameCounter = document.querySelector('.counter');
 let i = 0;
+let timeInterval = 1000;
 
 function getXPositionOfElement() {
-  let xPosition = Math.floor(Math.random() * gameWindow.clientWidth);
+  const xPosition = Math.floor(Math.random() * gameWindow.clientWidth);
   return xPosition;
 }
 
 
 function getYPositionOfElement() {
-  let yPosition = Math.floor(Math.random() * gameWindow.clientHeight);
+  const yPosition = Math.floor(Math.random() * gameWindow.clientHeight);
   return yPosition;
 }
 
 
-function del(item) {
+function deleteItem(item) {
   item.classList.remove('circle');
   item.remove();
 }
 
-function random() {
+function getGameLevel() {
+  switch(i) {
+    case 20:
+      timeInterval -= 200;
+    break;
+    case 40:
+      timeInterval -= 200;
+    break;
+    case 60:
+      timeInterval -= 100;
+      break;
+  }
+}
+
+
+
+function showElements() {
   const element = document.createElement('div');
   element.style.top = `${getYPositionOfElement()}px`;
   element.style.left = `${getXPositionOfElement()}px`;
   element.classList.add('circle');
   gameWindow.append(element);
-  setTimeout(del, 4000, element);
+  setTimeout(deleteItem, 4000, element);
 
-  element.addEventListener('click', () => {
+  element.addEventListener('mousedown', () => {
     i++;
-    console.log(i);
-    del(element);
+    gameCounter.textContent = i;
+    getGameLevel();
+    deleteItem(element);
   });
+  setTimeout(showElements, timeInterval);
 }
 
-
-
-setInterval(random, 1000);
-
-
-
-
- 
+showElements();
